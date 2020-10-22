e5c233e87dc58ab28103108436e9d88615154d26
jtruo <jazure622@gmail.com>
2020-10-22 16:57:00 -0400

    Added Class model

--- planner_proj/pandemic_app/models.py
+++ planner_proj/pandemic_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class UserAccount(models.Model):
@@ -7,3 +8,7 @@ class UserAccount(models.Model):
     password = models.TextField()
     credit_hours = models.IntegerField()
 
+class Class(models.Model):
+    class_name = models.TextField()
+    credits = models.IntegerField()
+    students = ArrayField(models.TextField())
