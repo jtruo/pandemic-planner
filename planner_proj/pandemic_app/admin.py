c16f88b85e8e54c9b40125f618106ca0547e09c8
jtruo <jazure622@gmail.com>
2020-10-22 16:32:50 -0400

    Added class model, fixed info.txt url

--- planner_proj/pandemic_app/admin.py
+++ planner_proj/pandemic_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserAccount, Class
 
 # Register your models here.
+admin.site.register(UserAccount)
+admin.site.register(Class)
+
+
--- planner_proj/pandemic_app/models.py
+++ planner_proj/pandemic_app/models.py
@@ -1,14 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Test(models.Model):
 
     name = models.TextField()
-    
     attr = models.TextField()
 
 class UserAccount(models.Model):
+
     username = models.TextField()
     email = models.TextField()
     password = models.TextField()
     credit_hours = models.IntegerField()
+
+class Class(models.Model):
+
+    class_name = models.TextField()
+    credits = models.IntegerField()
+    students = ArrayField(models.TextField())
+
+    
+
