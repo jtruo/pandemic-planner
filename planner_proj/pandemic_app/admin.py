4a92ea568a722a85a69472e19b76732e9d5657d0
jtruo <jazure622@gmail.com>
2020-10-22 17:06:36 -0400

    Added an admin page for viewing the data being entered into the database

--- planner_proj/pandemic_app/admin.py
+++ planner_proj/pandemic_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pandemic_app.models import UserAccount, Class
+
+admin.site.register(UserAccount)
+admin.site.register(Class)
 
 # Register your models here.
+
